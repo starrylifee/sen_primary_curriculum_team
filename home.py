2497bc4b3c2b445790a7f8bc18236052e2a80b1e
starrylife <forinnocen@naver.com>
2024-08-20 20:21:44 +0900

    home

---
+++ home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Streamlit의 기본 메뉴와 푸터 숨기기
+hide_github_icon = """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK{ display: none; }
+    #MainMenu{ visibility: hidden; }
+    footer { visibility: hidden; }
+    header { visibility: hidden; }
+    </style>
+"""
+
+st.markdown(hide_github_icon, unsafe_allow_html=True)
